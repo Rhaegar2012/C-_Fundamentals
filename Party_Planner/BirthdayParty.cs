08003593e7eb608ed2788f8b04cc364b58f01f16
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2020-12-04 05:52:23 -0500

    Add files via upload

---
+++ Party_Planner/BirthdayParty.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_Planner
+{
+    class BirthdayParty:Party
+    {
+        
+        public string CakeWriting { get; set; }
+        public bool CakeWritingTooLong
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private int ActualLength
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();
+                else
+                    return CakeWriting.Length;
+            }
+        }
+        public override decimal Cost
+        {
+            get
+            {
+                decimal totalCost = base.Cost;
+                decimal cakeCost;
+                if (CakeSize() == 8)
+                    cakeCost = 40M + ActualLength * .25M;
+                else
+                    cakeCost = 75M + ActualLength * .25M;
+                return totalCost + cakeCost;
+            }
+        }
+        //Constructor
+        public BirthdayParty(int numberOfPeople,
+            bool fancyDecorations, string cakeWriting)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+            {
+                return 8;
+            }
+            else
+            {
+                return 16;
+            }
+        }
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+            {
+                return 16;
+            }
+            else
+            {
+                return 40;
+            }
+        }
+        
+    }
+}
---
+++ Party_Planner/DinnerParty.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_Planner
+{
+    class DinnerParty:Party
+    {
+      
+        public bool HealthyOption { get; set; }
+ 
+        public override decimal Cost
+        {
+            // The get will return the value, it can also be used as a method to assign
+            //value without set , Use set when the variable needs to be accesed and modified  by someone else
+            get
+            {
+                decimal totalCost = base.Cost;
+                totalCost += (CalculateCostOfBeveragesPerPerson() * NumberOfPeople);
+                if (HealthyOption)
+                {
+                    totalCost *= 0.95M;
+                }
+                Console.WriteLine("DINNER COST" + totalCost);
+                return totalCost;
+            }
+     
+        }
+        // Class constructor , initializes NumberOfPeople, FancyDecorations  and HealthyOption
+        public DinnerParty(int numberOfPeople,bool healthyOption,bool fancyDecorations)
+        {
+            NumberOfPeople = numberOfPeople;
+            HealthyOption = healthyOption;
+            FancyDecorations = fancyDecorations;
+        }
+        
+       
+
+        public decimal CalculateCostOfBeveragesPerPerson()
+        {
+            decimal costOfBeveragesPerPerson;
+            if (HealthyOption)
+            {
+                costOfBeveragesPerPerson= 5.00M;
+            }
+            else
+            {
+                costOfBeveragesPerPerson= 20.00M;
+            }
+            return costOfBeveragesPerPerson;
+        }
+
+    }
+}
